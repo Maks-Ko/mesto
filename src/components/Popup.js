79d3bc71e1bc506fe6cddf64ca23850cdd29096b
Maksim Kozhevnikov <kozhevnikow@yandex.ru>
2021-06-25 23:13:37 +0500

    fix: function handleEscClose and handleOverlayClick

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,18 +3,21 @@ import { keyEscape } from '../utils/constants.js';
 export default class Popup {
     constructor({ popupSelector }) {
         this._popupSelector = popupSelector;
+        this._handleEscClose = this._handleEscClose.bind(this);
+        this._handleOverlayClick = this._handleOverlayClick.bind(this);
+        //this.close = this.close.bind(this);
     }
     
     open() {
         this._popupSelector.classList.add('popup_is-opened');
-        document.addEventListener('keydown', this._handleEscClose.bind(this));
-        //document.addEventListener('click', this._handleOverlayClick);
+        document.addEventListener('keydown', this._handleEscClose);
+        this._popupSelector.addEventListener('click', this._handleOverlayClick);
     }
 
     close() {
         this._popupSelector.classList.remove('popup_is-opened');
-        document.removeEventListener('keydown', this._handleEscClose.bind(this));
-        //document.removeEventListener('click', this._handleOverlayClick);
+        document.removeEventListener('keydown', this._handleEscClose);
+        this._popupSelector.removeEventListener('click', this._handleOverlayClick);
     }
 
     // метод закрытия попапа нажатием на Esc
@@ -25,12 +28,11 @@ export default class Popup {
     }
 
     // закрытия попапа кликом на оверлей
-    // _handleOverlayClick(evt) {
-    //     if (evt.target === evt.currentTarget) {
-    //         console.log("click");
-    //         //this.close.bind(this);
-    //     }
-    // }
+    _handleOverlayClick(evt) {
+        if (evt.target === evt.currentTarget) {
+            this.close();
+        }
+    }
 
     // метод добавления слушателей
     setEventListeners() {
--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -4,13 +4,12 @@ import Popup from './Popup.js';
 export default class PopupWithImage extends Popup {
     constructor({ popupSelector }) {
        super({ popupSelector });
-    }    
+    }
 
     open(name, link) {
-        this._popupSelector.classList.add('popup_is-opened');
+        super.open();
         popupImageTitle.textContent = name;
         popupImageContent.src = link;
         popupImageContent.alt = name;
-        document.addEventListener('keydown', this._handleEscClose.bind(this));
     }
 }
